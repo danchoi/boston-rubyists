9c6f959e7e3549ad6225ae3423ce165ecb6d5fc3
Daniel Choi <dhchoi@gmail.com>
2012-02-01 20:40:20 -0500

    some fixes

--- boston_rubyists.rb
+++ boston_rubyists.rb
@@ -7,14 +7,16 @@ DB = Sequel.connect "postgres:///bostonruby", logger: Logger.new(STDERR)
 class BostonRubyists < Sinatra::Base
 
   get('/') {
-    ds = DB[:updates].order(:date.desc).limit(400).
-      filter("date < now() - interval '5 hour'")
-    @updates = ds.to_a[0,3]
+    ds = DB[:updates].order(:date.desc).limit(100)
+    @updates = ds.to_a.map {|x| 
+      x[:content] = x[:content].gsub(/^/, ' ' * 6).gsub(/href="\//, 'href="https://github.com/') 
+      x
+    }
     erb :index 
   }
 
   get('/updates') {
-    ds = DB[:updates].order(:date.desc).limit(400)
+    ds = DB[:updates].order(:date.desc).limit(100)
     @updates = ds.filter("date > ?", params[:from_time]).to_a
     @updates.to_json
   }
